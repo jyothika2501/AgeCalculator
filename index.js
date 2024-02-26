6f7bc3be0e2730a2bf04f4868f9eb19bea1e457e
Jyothika Bellamkond <bjyothika25@gmail.com>
2024-02-26 22:19:11 +0530

    validations for age in index.js file

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 function calculateAge() {
     const dobInput = document.getElementById("dob").value; // .value gives the exact value of the date given in the input
     const dobObject = new Date(dobInput); //instance of date object
+    if(dobInput === ""){
+        alert("Please enter your age");
+    }
+    else{
+        const currentDate = new Date(); //prints the currentdate and time
 
-    const currentDate = new Date(); //prints the currentdate and time
+        const ageDifference = currentDate - dobObject;
 
-    const ageDifference = currentDate - dobObject;
-
-    const age = Math.round( (ageDifference) / (1000 * 3600 * 24 * 365.25) );
-    document.getElementById("output").innerHTML = "Your age is " + age + " year old";
+        const age = Math.round( (ageDifference) / (1000 * 3600 * 24 * 365.25) );
+        document.getElementById("output").innerHTML = "Your age is " + age + " year old";
+    }
 }
 
